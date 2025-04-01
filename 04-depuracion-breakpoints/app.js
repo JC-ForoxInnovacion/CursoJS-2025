343cab6e117bd7c58466e9dd2563319e0b486038
JC <jc.foroxinnovacion@gmail.com>
2025-04-01 13:10:48 +0200

    Depuración y Breakpoints

---
+++ 04-depuracion-breakpoints/app.js
@@ -0,0 +1,28 @@
+let a = 10;
+let b = 15;
+let c = 20;
+let d = 25;
+
+console.log('a:', a);
+console.info('b:', b);
+console.warn('c:', c);
+console.error('d:', d);
+
+// Breakpoint en Google Chrome
+// - Seleccionar SOURCES de las herramisntas de desarrollador
+// - Seleccionar el .js correspondiente
+// - Hacer clic en el número de linea en donde se insertará el breakpoint
+
+// Ver imagen google-chrome-breakpoint.png
+
+
+// Breakpoint en Visual Studio Code
+// - Hacer clic en el punto rojo a la izquierda del número de linea en donde se insertará el breakpoint
+// Iniciar la ejecución tecla F5 o en el menú Run - Start Debbuging - Seleccionar node.js
+
+// NOTA: Puedes eliminar un breakpoint haciendo click nyevamente sobre el
+
+// Ver imágenes:
+//    vscode-breakpoint-1.png
+//    vscode-breakpoint-2.png
+//    vscode-breakpoint-3.png
