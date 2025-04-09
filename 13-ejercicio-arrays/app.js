18dfc1bfbe389b38b0c0a33e02b2f59d2cfcc70e
JC <jc.foroxinnovacion@gmail.com>
2025-04-09 09:48:43 +0200

    ejercicio-arrays

---
+++ 13-ejercicio-arrays/app.js
@@ -0,0 +1,25 @@
+let miObj = {
+  arr: [
+    [1, 2, 3, 4],  // Primer arreglo
+    [5, 6, 7, 8],  // Segundo arreglo
+    [9, 10, 11, 12], // Tercer arreglo
+    [13, 14, 15, 16], // Cuarto arreglo
+    [17, 18, 19, 20], // Quinto arreglo
+    [21, 22, 23, 24], // Sexto arreglo
+    [25, 26, 27, 28], // Séptimo arreglo
+    [29, 30, 31, 32], // Octavo arreglo
+    [33, 34, 35, 36], // Noveno arreglo
+    [37, 38, 39, 40], // Décimo arreglo
+  ]
+};
+
+console.log({miObj});
+
+let matriz4 = [
+  miObj.arr[0],
+  miObj.arr[3],
+  miObj.arr[5],
+  miObj.arr[7]
+];
+
+console.log({matriz4});
